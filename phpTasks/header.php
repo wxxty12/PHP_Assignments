4e8ba3b892b0de16e71ea89e28e688f1fa10c1df
Xingxing Wang <xingxing23000@student.hamk.fi>
2024-02-07 14:53:19 +0200

    Bug fix

--- phpTasks/header.php
+++ phpTasks/header.php
@@ -28,7 +28,7 @@
     </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="site.css">
+    <link rel="stylesheet" href="layout/site.css">
     <!-- To team member: Below is the page Specific css. Replace it with your own css. -->
     <!-- <link rel="stylesheet" href="menu.css"> -->
 </head>
